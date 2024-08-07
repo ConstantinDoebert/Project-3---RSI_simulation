fe750610e2236186131bdeba569a3d184b487ba8
Constantin Doebert <doebert.constantin@gmail.com>
2024-08-07 22:34:11 +0200

    completed rsi

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import numpy as np
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 
-key = ""
+key = "eba7da3b24104ca594f061cb762cb8da"
 
 
-def get_closing_prices(ticker: str, interval=14):
+def get_closing_prices(ticker: str, interval=30):
     """
-    Get closing prices of recent 14 days.
+    Get closing prices of recent 30 days.
     """
     date_today = datetime.today().date() # end date of time series
-    start_date = date_today - timedelta(np.ceil(interval * 7/5))
+    start_date = date_today - timedelta(np.ceil(interval * 7/5)) # adjusting delta for trading days; 5 out of 7 days
 
     response = rq.get(
             f"https://api.twelvedata.com/time_series",
@@ -33,9 +33,9 @@ def get_closing_prices(ticker: str, interval=14):
         closing_prices.append(float(value["close"]))
 
     df = pd.DataFrame(closing_prices, dates).iloc[::-1]
-    df.rename(columns={0: "Last 14 days closing"}, inplace=True)
+    df.rename(columns={0: f"Last {interval} days closing"}, inplace=True)
 
-    return df
+    return df, interval
 
 
 def plot_values(ticker: str):
@@ -44,15 +44,27 @@ def plot_values(ticker: str):
     plt.show()
 
 
-
 def rsi(ticker: str):
-    df = get_closing_prices(ticker)
+    closing_prices = get_closing_prices(ticker)
+    df = closing_prices[0]
+    interval = closing_prices[1]
 
-    for value in df["Last 14 days closing"]:
-        u, d = ([], [])
-        u.append(value)
+    df["U"] = 0
+    df["D"] = 0
 
+    for i in range(1, len(df)):
+        # # see: hhttps://en.wikipedia.org/wiki/Relative_strength_index#Calculation
+        diff = df.iloc[i][f"Last {interval} days closing"] - df.iloc[i-1][f"Last {interval} days closing"]
+        if diff > 0:
+            df.at[df.index[i], "U"] = diff
+        else:
+            df.at[df.index[i], "D"] = abs(diff)
 
-    return u
+    df["EMA_U"] = df["U"].ewm(span=14, adjust=False).mean() # calculate exponential moving average
+    df["EMA_D"] = df["D"].ewm(span=14, adjust=False).mean()
+    df["RS"] = df["EMA_U"] / df["EMA_D"]
+    df["RSI"] = 100 - 100 / (1 + df["RS"]) # see: hhttps://en.wikipedia.org/wiki/Relative_strength_index#Calculation
+
+    return df
 
 print(rsi("NVDA"))
